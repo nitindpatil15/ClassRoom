fcb4fed2adccebd7a51d2f46fe504e4bb7e6b384
nitindpatil15 <nitindpatil2002@gmail.com>
2024-08-13 19:52:11 +0530

    cors origin

--- classroombackend/src/app.js
+++ classroombackend/src/app.js
@@ -5,46 +5,39 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5174",
+  origin: '*', // Set your specific domain here if using credentials
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  headers: ["Content-Type", 'Authorization', 'auth-token'],
+  allowedHeaders: ["Content-Type", 'Authorization', 'auth-token'],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, auth-token');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-// Config settings In CORS() middleware
-// for setting limits on data
+// Config settings in CORS middleware for setting limits on data
 app.use(express.json({ limit: "50mb" }));
 
-// parse incoming requests in url of browser
+// Parse incoming requests in the URL of the browser
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-// for any kind of static files, public is folder
+// Serve static files from the public folder
 app.use(express.static("public"));
 
-// Accessing cookies from user browser which can only  be accessed by server side code using the following method
+// Access cookies from the user browser
 app.use(cookieParser());
 
 // Routes import
-import authRoutes from "./Routes/authRoutes.js"
-import principalRoutes from "./Routes/principalRoutes.js"
-import teacherRoutes from "./Routes/teacherRoutes.js"
-import dashboardRoutes from "./Routes/dadhboardRoute.js"
+import authRoutes from "./Routes/authRoutes.js";
+import principalRoutes from "./Routes/principalRoutes.js";
+import teacherRoutes from "./Routes/teacherRoutes.js";
+import dashboardRoutes from "./Routes/dashboardRoute.js";
 import { createPrincipal } from "./controllers/auth.controller.js";
 
 createPrincipal();
 
-// Router Declaration
+// Router declaration
 app.use('/api/v1/dashboard', dashboardRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/principal', principalRoutes);
-app.use('/api/v1/teacher',teacherRoutes);
+app.use('/api/v1/teacher', teacherRoutes);
 
 export { app };
