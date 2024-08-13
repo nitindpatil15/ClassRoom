b214937e0239074be4e5b355f754bf7525214c02
nitindpatil15 <nitindpatil2002@gmail.com>
2024-08-13 16:52:02 +0530

    backend

--- classroombackend/src/app.js
+++ classroombackend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5174",
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   headers: ["Content-Type", 'Authorization', 'auth-token'],
   credentials: true,
